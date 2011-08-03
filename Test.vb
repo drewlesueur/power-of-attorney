43eb4b4d8f7e4bf0f0e1b7cda0eabeeb97671d03
Drew LeSueur <drewalex@gmail.com>
2011-08-03 03:54:42 -0500

    print screen can now save an image

--- Test.vb
+++ Test.vb
@@ -1,7 +1,9 @@
-'C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319>vbc.exe /t:exe /out:.\Test1.exe C:\Documents and Settings\Drew LeSueur\workspace\lcc\power-of-attorney\Test.vb"
+'C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319>vbc.exe /t:exe /out:.\printscreen.  exe "C:\Documents and Settings\Drew LeSueur\workspace\lcc\power-of-attorney\Test .vb"
+
 
 ' Allow easy reference to the System namespace classes.
 Imports System
+Imports Clipboard = System.Windows.Forms.Clipboard
 ' This module houses the application's entry point.
 Public Module modmain
    ' Main is the application's entry point.
@@ -12,6 +14,11 @@ Public Module modmain
      'System.Windows.Forms.SendKeys.Send("%{PRTSC}")
      System.Threading.Thread.Sleep(1000)
      System.Windows.Forms.SendKeys.SendWait("%{PRTSC}")
+     Dim image As System.Drawing.Image = Nothing
+     If Clipboard.ContainsImage() Then
+       image = Clipboard.GetImage()
+       image.Save("c:\mybitmap.png")
+     End If
 
    End Sub
 End Module
