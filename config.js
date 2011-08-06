f497a05c569bfc572306500705eaf4202ac0a897
Drew LeSueur <drewalex@gmail.com>
2011-08-06 13:51:43 -0500

    power of attorney done

--- config.js
+++ config.js
@@ -29,13 +29,29 @@ module.exports = {
     borrowerZip: [borrowerImage, [26, 7, 10, 1]],
     borrowerCity: [borrowerImage, [26, 8, 12, 1]],
     borrowerState: [borrowerImage, [26, 9, 12, 1]],
+    borrowerLicence: [borrowerImage, [26, 17, 20, 1]],
+    borrowerDOB: [borrowerImage, [26, 20, 20, 1]],
+
+    vin: [carImage, [24, 15, 20, 1]],
+    year: [carImage, [24, 9, 5, 1]],
+    make: [carImage, [24, 10, 17, 1]],
+    bodyStyle: [carImage, [24, 13, 17, 1]],
   },
   forms : {
     powerOfAttorney: {
       background: "poa.png",
       images: {
         borrowerName: [63, 322 /*, width, height*/],
-        borrowerAddress: [63, 355]
+        borrowerAddress: [63, 355],
+        borrowerCity: [481, 355],
+        borrowerState: [656, 355],
+        borrowerZip: [696, 355],
+        borrowerLicence: [482, 322],
+        borrowerDOB: [633, 323],
+        vin: [63, 164],
+        year: [438, 164],
+        make: [479, 164],
+        bodyStyle: [637, 164],
       }
     }
   },
